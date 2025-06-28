437e2951ade7649fc8d3f6e879ed0989bf4c5f09
akhdrzki <akhadirizky@gmail.com>
2025-06-28 21:22:00 +0800

    feat: add neovim config

---
+++ config/nvim/lua/plugins/init.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  { import = "nvchad.blink.lazyspec" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+        "typst",
+      },
+    },
+  },
+
+  -- hlchunk
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require "configs.chunk"
+    end,
+  },
+
+  -- presence nvim
+  {
+    "jiriks74/presence.nvim",
+    event = "UIEnter",
+    config = function()
+      require "configs.discord"
+    end,
+    --opts = {},
+  },
+
+  -- typst preview
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "1.*",
+    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+    config = function()
+      require "configs.typst"
+    end,
+  },
+
+  -- inline diagnostics
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require "configs.diagnostic"
+    end,
+  },
+}
